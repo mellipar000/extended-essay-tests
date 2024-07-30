13b766864e7b3717e0905f85b9f3d91a92a0ea26
Parker Melling <144739292+mellipar000@users.noreply.github.com>
2024-07-30 18:42:53 -0400

    Model Saving and Choice Updates

--- comparison.py
+++ comparison.py
@@ -8,6 +8,11 @@ from sklearn.tree import DecisionTreeClassifier
 import time
 import ssl
 from memory_profiler import memory_usage
+import joblib
+import os
+
+# Supress Tensorflow logging messages
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # Function to train the model
 def train_model(model, x_train: np.ndarray, y_train: np.ndarray) -> np.ndarray:
@@ -22,6 +27,15 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 # In place to prevent multiprocessing errors due to the memory profiler
 if __name__ == '__main__':
+  
+  # Asking the user if they would like to attempt a quick execution of the program
+  while True:
+    choice = input('Would you like to attempt a quick execution of the program? (y/n): ')
+    if (choice in ['y', 'n']):
+      break
+    else:
+      print('Invalid input. Please try again.')
+  
   # Starting a program timer
   start_time = time.time()
 
@@ -45,19 +59,54 @@ if __name__ == '__main__':
   # Reshaping the labels to 1d arrays since sklearn expects 1d arrays
   y_train = y_train.ravel()
   y_test = y_test.ravel()
+  
+  # Path to the machine learning models - replace with your own path
+  save_dir = 'C:/Users/parke/Downloads/machine-learning-models/'
 
-  # Creating an instance of the RandomForestClassifier
-  rf = RandomForestClassifier()
+  # Path to the random forest model and training time
+  rf_path = os.path.join(save_dir, 'random_forest_model.joblib')
+  rf_training_time_path = os.path.join(save_dir, 'random_forest_training_time.txt')
+  rf_training_memory_path = os.path.join(save_dir, 'random_forest_training_memory.txt')
 
-  # Starting a timer for the RandomForestClassifier training
-  rf_train_start_time = time.time()
+  # Checking if the random forest model already exists and if the user wants to use it
+  if (choice == 'y' and os.path.exists(rf_path) and os.path.exists(rf_training_time_path) and os.path.exists(rf_training_memory_path)):
+     
+     # Loading the random forest model
+     rf = joblib.load(rf_path)
+     
+     # Loading the training time
+     with open(rf_training_time_path, 'r') as f:
+       rf_train_time = float(f.read())
+       
+     # Loading the training memory
+     with open(rf_training_memory_path, 'r') as f:
+       rf_training_memory = float(f.read())
 
-  # Training the RandomForestClassifier and measuring memory usage
-  rf_training_memory = memory_usage((train_model, (rf, x_train2, y_train)), max_usage=True)
+  else:
 
-  # Ending the RandomForestClassifier training timer
-  rf_train_end_time = time.time()
+    # Creating an instance of the RandomForestClassifier
+    rf = RandomForestClassifier()
 
+    # Starting a timer for the RandomForestClassifier training
+    rf_train_start_time = time.time()
+
+    # Training the RandomForestClassifier and measuring memory usage
+    rf_training_memory = memory_usage((train_model, (rf, x_train2, y_train)), max_usage=True)
+    
+    # Ending the RandomForestClassifier training timer
+    rf_train_time = time.time() - rf_train_start_time
+
+    # Saving the RandomForestClassifier model
+    joblib.dump(rf, rf_path)
+    
+    # Saving the training time
+    with open(rf_training_time_path, 'w') as f:
+      f.write(str(rf_train_time))
+    
+    # Saving the training memory
+    with open(rf_training_memory_path, 'w') as f:
+      f.write(str(rf_training_memory))
+    
   # Starting a timer for the RandomForestClassifier prediction
   rf_prediction_start_time = time.time()
 
@@ -74,22 +123,54 @@ if __name__ == '__main__':
   print('\nOverall Random Forest Accuracy:', accuracy_score(y_pred_rf, y_test) * 100, '%\n')
   print('Random Forest Classification Report:\n', classification_report(y_pred_rf, y_test))
   print('Random Forest Confusion Matrix:\n', confusion_matrix(y_pred_rf, y_test))
-  print(f'\nRandom Forest Training Time: {rf_train_end_time - rf_train_start_time} seconds')
+  print(f'\nRandom Forest Training Time: {rf_train_time} seconds')
   print(f'Random forest Prediction Time: {rf_prediction_end_time - rf_prediction_start_time} seconds')
   print(f'Random Forest Training Memory: {rf_training_memory} MB')
   print(f'Random Forest Prediction Memory: {rf_prediction_memory} MB')
 
-  # Creating an instance of the DecisionTreeClassifier
-  dtc = DecisionTreeClassifier()
+  # Path to the decision tree model
+  dtc_path = os.path.join(save_dir, 'decision_tree_model.joblib')
+  dtc_training_time_path = os.path.join(save_dir, 'decision_tree_training_time.txt')
+  dtc_training_memory_path = os.path.join(save_dir, 'decision_tree_training_memory.txt')
 
-  # Starting a timer for the DecisionTreeClassifier training
-  dtc_train_start_time = time.time()
-
-  # Training the DecisionTreeClassifier and measuring memory usage
-  dtc_training_memory = memory_usage((train_model, (dtc, x_train2, y_train)), max_usage=True)
-
-  # Ending the DecisionTreeClassifier training timer
-  dtc_train_end_time = time.time()
+  # Checking if the decision tree model already exists and if the user wants to use it
+  if (choice == 'y' and os.path.exists(dtc_path) and os.path.exists(dtc_training_time_path) and os.path.exists(dtc_training_memory_path)):
+     
+    # Loading the decision tree model
+    dtc = joblib.load(dtc_path)
+     
+    # Loading the training time
+    with open(dtc_training_time_path, 'r') as f:
+      dtc_train_time = float(f.read())
+    
+    # Loading the training memory
+    with open(dtc_training_memory_path, 'r') as f:
+      dtc_training_memory = float(f.read())
+      
+  else:
+    
+    # Creating an instance of the DecisionTreeClassifier
+    dtc = DecisionTreeClassifier()
+     
+    # Starting a timer for the DecisionTreeClassifier training
+    dtc_train_start_time = time.time()
+     
+    # Training the DecisionTreeClassifier and measuring memory usage
+    dtc_training_memory = memory_usage((train_model, (dtc, x_train2, y_train)), max_usage=True)
+     
+    # Ending the DecisionTreeClassifier training timer
+    dtc_train_time = time.time() - dtc_train_start_time
+     
+    # Saving the DecisionTreeClassifier model
+    joblib.dump(dtc, dtc_path)
+     
+    # Saving the training time
+    with open(dtc_training_time_path, 'w') as f:
+      f.write(str(dtc_train_time))
+    
+    # Saving the training memory
+    with open(dtc_training_memory_path, 'w') as f:
+      f.write(str(dtc_training_memory))
 
   # Starting a timer for the DecisionTreeClassifier prediction
   dtc_prediction_start_time = time.time()
@@ -107,7 +188,7 @@ if __name__ == '__main__':
   print('\nOverall Decision Tree Accuracy:', accuracy_score(y_pred_dtc, y_test) * 100, '%\n')
   print('Decision Tree Classification Report:\n', classification_report(y_pred_dtc, y_test))
   print('Decision Tree Confusion Matrix:\n', confusion_matrix(y_pred_dtc, y_test))
-  print(f'\nDecision Tree Training Time: {dtc_train_end_time - dtc_train_start_time} seconds')
+  print(f'\nDecision Tree Training Time: {dtc_train_time} seconds')
   print(f'Decision Tree Prediction Time: {dtc_prediction_end_time - dtc_prediction_start_time} seconds')
   print(f'Decision Tree Training Memory: {dtc_training_memory} MB')
   print(f'Decision Tree Prediction Memory: {dtc_prediction_memory} MB')
