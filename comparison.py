0aa7cd83791df69f176ef916c32d150064046c13
mellipar000 <mellipar000@hsestudents.org>
2024-10-17 01:35:36 -0400

    Finished Script

--- comparison.py
+++ comparison.py
@@ -2,9 +2,9 @@ from tensorflow import keras
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 import numpy as np
-import cv2
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
 import time
 import ssl
 from memory_profiler import memory_usage
@@ -14,7 +14,6 @@ import platform
 
 # Supress Tensorflow logging messages
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
-
 
 # Function to train the model
 def train_model(model, x_train: np.ndarray, y_train: np.ndarray) -> np.ndarray:
@@ -113,7 +112,7 @@ def find_directory(choice: str):
 
     # Paths to the machine learning models
     save_dirs = {
-        "School Laptop": "/Users/MELLIPAR000/Downloads/machine-learning-models/",
+        "School Laptop": "/Users/MELLIPAR000/Downloads/machine_learning_models/",
         "PC": "C:/Users/parke/Downloads/machine-learning-models/",
         "Your Computer": "Your_Path_Here",
     }
@@ -134,7 +133,7 @@ def find_directory(choice: str):
     return save_dir
 
 # Function to create a bar graph
-def create_bar_graph(ylabel, values, units, fontsize = 'large'):
+def create_special_bar_graph(ylabel, values, units, fontsize = 'large'):
     # Using the global variable num_plots to keep track of the number of subplots
     global num_plots
 
@@ -155,6 +154,18 @@ def create_bar_graph(ylabel, values, units, fontsize = 'large'):
     
     # Incrementing the number of subplots
     num_plots += 1
+
+def create_bar_graph(xlabel, ylabel, labels, values, units, fontsize = 'large'):
+    # Creating the bar graph
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(ylabel + " vs. " + xlabel)
+    plt.xticks(range(10), labels)
+
+    # Annotating the bars with the values
+    for index, value in enumerate(values):
+        plt.bar(index, value, 0.9)
+        plt.text(index, value / 2, str(round(value, 2)) + ' ' + units, ha = 'center', va = 'bottom', fontsize = fontsize, fontweight = 'bold')
 
 # Disabling SSL certificate verification (only leave this uncommented when on a school-managed device)
 ssl._create_default_https_context = ssl._create_unverified_context
@@ -208,7 +219,7 @@ if __name__ == "__main__":
         print("Training the Random Forest model...")
 
         # Creating an instance of the RandomForestClassifier
-        rf = RandomForestClassifier()
+        rf = RandomForestClassifier(n_jobs=-1)
 
         # Starting a timer for the RandomForestClassifier training
         rf_train_start_time = time.time()
@@ -252,10 +263,12 @@ if __name__ == "__main__":
     # Measuring memory usage for the prediction in MiB and converting to MB
     rf_prediction_memory = memory_usage((model_predict, (rf, x_test)), max_usage=True) * 1.04858
 
+    rf_confusion_matrix = confusion_matrix(y_test, y_pred_rf)
+
     # Printing the RandomForestClassifier performance metrics
     print("\nOverall Random Forest Accuracy:", round(accuracy_score(y_pred_rf, y_test) * 100, 2), "%\n",)
     print("Random Forest Classification Report:\n", classification_report(y_pred_rf, y_test),)
-    print("Random Forest Confusion Matrix:\n", confusion_matrix(y_pred_rf, y_test))
+    print("Random Forest Confusion Matrix:\n", rf_confusion_matrix)
     print(f"\nRandom Forest Training Time: {round(rf_train_time, 2)} seconds")
     print(f"Random forest Prediction Time: {round((rf_prediction_end_time - rf_prediction_start_time) * 1000, 2)} milliseconds")
     print(f"Random Forest Training Memory: {round(rf_training_memory, 2)} Mb")
@@ -336,10 +349,12 @@ if __name__ == "__main__":
     # Measuring memory usage for the prediction in MiB and converting to MB
     dtc_prediction_memory = memory_usage((model_predict, (dtc, x_test)), max_usage = True) * 1.04858
 
+    dtc_confusion_matrix = confusion_matrix(y_test, y_pred_dtc)
+
     # Printing the DecisionTreeClassifier performance metrics
     print("\nOverall Decision Tree Accuracy:", round(accuracy_score(y_pred_dtc, y_test) * 100, 2), "%\n")
     print("Decision Tree Classification Report:\n", classification_report(y_pred_dtc, y_test))
-    print("Decision Tree Confusion Matrix:\n", confusion_matrix(y_pred_dtc, y_test))
+    print("Decision Tree Confusion Matrix:\n", dtc_confusion_matrix)
     print(f"\nDecision Tree Training Time: {round(dtc_train_time, 2)} seconds")
     print(f"Decision Tree Prediction Time: {round((dtc_prediction_end_time - dtc_prediction_start_time) * 1000, 2)} milliseconds")
     print(f"Decision Tree Model Size: {round(dtc_model_size, 2)} Mb")
@@ -366,15 +381,55 @@ if __name__ == "__main__":
     num_plots = 1
 
     # Creating bar graphs for the model sizes, training times, prediction accuracies, predictions per second, training memories, and prediction memories
-    create_bar_graph("Model Size (MB)", model_sizes, 'MB')
-    create_bar_graph("Training Time (seconds)", training_times, 'sec')
-    create_bar_graph("Prediction Accuracy (%)", prediction_accuracies, '%')
-    create_bar_graph("# Predictions Per Second (in thousands)", predictions_per_sec, '* 10^3 pps', 'medium')
-    create_bar_graph("Training Memory (MB)", training_memories, 'MB')
-    create_bar_graph("Prediction Memory (MB)", prediction_memories, 'MB')
+    create_special_bar_graph("Model Size (MB)", model_sizes, 'MB')
+    create_special_bar_graph("Training Time (seconds)", training_times, 'sec')
+    create_special_bar_graph("Prediction Accuracy (%)", prediction_accuracies, '%')
+    create_special_bar_graph("# Predictions Per Second (in thousands)", predictions_per_sec, '* 10^3 pps', 'medium')
+    create_special_bar_graph("Training Memory (MB)", training_memories, 'MB')
+    create_special_bar_graph("Prediction Memory (MB)", prediction_memories, 'MB')
 
     # Adjusting the spacing between the subplots
     plt.subplots_adjust(hspace = 0.3, wspace = 0.3)
 
     # Displaying the bar graphs
     plt.show()
+
+    # Labels with index corresponding to the image class
+    labels = ["Airplane", "Automobile", "Bird", "Cat", "Deer", "Dog", "Frog", "Horse", "Ship", "Truck"]
+
+    # Creating lists to store the model accuracies for each image class
+    rf_class_accuracies = rf_confusion_matrix.diagonal() / 10
+    dtc_class_accuracies = dtc_confusion_matrix.diagonal() / 10
+
+    # Creating a figure to display the bar graph
+    plt.figure()
+
+    # Creating a bar graph for the random forest classifier
+    create_bar_graph("Image Class", "Accuracy", labels, rf_class_accuracies, '%')
+
+    # Displaying the bar graph
+    plt.show()
+
+    # Creating a figure to display the bar graph
+    plt.figure()
+
+    # Creating a bar graph for the decision tree classifier
+    create_bar_graph("Image Class", "Accuracy", labels, dtc_class_accuracies, '%')
+    
+    # Displaying the bar graph
+    plt.show()
+
+    # Annotating the bars with the values
+    for index, value in enumerate(rf_class_accuracies):
+        plt.text(index, value / 2, str(round(value, 2)) + '%', ha = 'center', va = 'bottom', fontsize = 'medium', fontweight = 'bold')
+
+    # Displaying the decision tree
+    tree.plot_tree(dtc, filled = True, fontsize = 6)
+    plt.show()
+
+    # Plot first decision tree in the random forest classifier
+    tree.plot_tree(rf.estimators_[0], filled = True, fontsize = 6)
+    plt.show()
+
+    # Creating lists for model accuracies for each image class
+    
